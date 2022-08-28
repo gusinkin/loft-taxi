4c9dde2a634af27a42363aa1b52d99634adac903
Dmitry Gusinkin <d.gusinkin@gmail.com>
2022-08-28 20:56:57 +0300

    tests not added :(

---
+++ src/App.spec.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import App from './App';
+
+jest.mock('./pages/LoginPage', () => ({
+  LoginPageWithAuth: () => <div>Login Page</div>,
+}));
+jest.mock('./pages/MapPage', () => ({
+  LoginPageWithAuth: () => <div>Map Page</div>,
+}));
+jest.mock('./pages/ProfilePage', () => ({
+  LoginPageWithAuth: () => <div>Profile Page</div>,
+}));
+
+describe('App', () => {
+  it('renders correctly', () => {
+    const { container } = render(<App />);
+    expect(container.innerHTML).toMatch('Login Page');
+  });
+
+  describe('when clicked on navigation buttons', () => {
+    describe('when logged out', () => {
+      it('shows login page', () => {
+        const { getByText, container } = render(<App />);
+        fireEvent.click(getByText('Карта'));
+        expect(container.innerHTML).toMatch('Login Page');
+        fireEvent.click(getByText('Профиль'));
+        expect(container.innerHTML).toMatch('Login Page');
+      });
+    });
+
+    describe('when logged in', () => {
+      it('opens the corresponding page', () => {
+        const { getByText, container } = render(<App />);
+        fireEvent.click(getByText('Карта'));
+        expect(container.innerHTML).toMatch('Map Page');
+        fireEvent.click(getByText('Профиль'));
+        expect(container.innerHTML).toMatch('Profile Page');
+      });
+    });
+  });
+});
---
+++ src/pages/LoginPage.spec.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { LoginPageWithAuth } from './LoginPage';
+import { render } from '@testing-library/react';
+
+describe('LoginPage', () => {});
